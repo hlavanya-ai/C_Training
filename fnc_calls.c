e0b967a2ac0a4b5be2286b129e571ebd093e966d
hlavanya-ai <lathahanumantharaya2@gmail.com>
2025-11-22 08:49:39 +0530

    Add files via upload

---
+++ fnc_calls.c
@@ -0,0 +1,24 @@
+
+#include<stdio.h>
+void sum_value(int a,int b)
+{
+    int sum=a+b;
+    printf("%d",sum);
+
+}
+void sum_ref(int *a,int *b)
+
+{
+    int sum=*a+*b;
+    printf("%d ",sum);
+
+}
+void main()
+{
+    int a=60;
+    int b=30;
+    printf("call by value :\n");
+    sum_value(a,b);//call by value
+    printf("call by reference:\n");
+    sum_ref(&a, &b);//call by ref
+}
